1847b6daf1f432fb06d5de5361cd24885066d239
vsparrow <khan@vsparrow.com>
2018-05-18 09:20:50 -0400

    on user#create force param length

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,17 @@ class UsersController < ActionController::API
 
   def create
     if params[:firstname] && params[:lastname] && params[:email]  && params[:password]
-      firstname = params[:firstname]
-      lastname = params[:lastname]
-      email = params[:email]
-      password = params[:password]
-      render json: {"ok": "we made it to create"}
+      if params[:firstname].length > 0 && params[:lastname].length > 0 && params[:email].length > 0  && params[:password].length > 0
+        firstname = params[:firstname]
+        lastname = params[:lastname]
+        email = params[:email]
+        password = params[:password]
+        render json: {"ok": "we made it to create"}
+      else
+        render json: {"error": "Missing create data, too small"}
+      end
     else
-      render json: {"error": "Missing create data"}
+      render json: {"error": "Missing create data, missing params"}
     end #if
   end #create
 end#class
