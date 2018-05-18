a919eab9c519413a196166bc4f1a3306834dbe65
vsparrow <khan@vsparrow.com>
2018-05-18 02:49:04 -0400

    bypass auth for signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,26 @@
-class UsersController < ApplicationController
+# class UsersController < ApplicationController
+class UsersController < ActionController::API
   #remove after testing ##############################
-  def index
-    @Users = User.all
-    # render json: "HELLO"  #works
-    object = {user: {data: 1, moredata: 2}}
-    render json: object                       #also works
-  end
 
-  def new
-  end
+  # def index
+  #   @Users = User.all
+  #   # render json: "HELLO"  #works
+  #   object = {user: {data: 1, moredata: 2}}
+  #   render json: object                       #also works
+  # end
+  #
+  # def new
+  # end
 
   def create
-  end
-end
+    if params[:firstname] && params[:lastname] && params[:email]  && params[:password]
+      firstname = params[:firstname]
+      lastname = params[:lastname]
+      email = params[:email]
+      password = params[:password]
+      render json: {"ok": "we made it to create"}
+    else
+      render json: {"error": "Missing create data"}
+    end #if
+  end #create
+end#class
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users' => 'users#index'
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
+  # get '/users' => 'users#index'
+  # get '/signup' => 'users#new'
+  # post '/users' => 'users#create'
+  post '/signup' => 'users#create'
   get '/reviews' => 'reviews#index'
   get '/reviews/:id' => 'reviews#show'
   post "/reviews" => "reviews#create"
